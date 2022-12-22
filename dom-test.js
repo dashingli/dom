50a1c1c76e761b0f7b4234a84f60655249369a06
lilinzhe <1120294646@qq.com>
2022-12-22 21:22:59 +0800

    dom库封装

---
+++ dom-test.js
@@ -0,0 +1,20 @@
+
+console.log('获取对应的元素\n')
+const div = dom.find('#test>.red')[0];
+console.log(div);
+
+console.log('获取多个 div.red 元素\n')
+const divList = dom.find('.red');
+console.log(divList);
+
+console.log('遍历 divList 里的所有元素\n')
+dom.each(divList);
+console.log('获取divList 里的指定元素\n')
+dom.each(divList,2);
+
+
+console.log('设置 div.style\n')
+const div2 = document.querySelector('.yellow');
+dom.style(div2,'font-size','50px');
+dom.style(div2,'color','red');
+dom.style(div2,'border','1px solid green');
---
+++ dom.js
@@ -0,0 +1,23 @@
+window.dom = {
+    find : (selector) => {
+        return document.querySelectorAll(selector);
+    },
+    style : (node,name,value) => {
+        if(node.nodeType === 1){
+            node.style[name] = value;
+        }else if(node.nodeType === 3){
+            console.log(这个是文本节点);
+        }
+    },
+    each : function (nodeList,n) {
+        const list = Array.from(nodeList);
+        if(arguments.length === 1){
+            for (let i = 0; i < nodeList.length; i++) {
+                console.log(list[i]);
+            }
+        }else if(arguments.length === 2){
+            console.log(list[n]);
+        }
+
+    }
+}
